00af3fbdda29b80c7d07ec86cacb946447225923
Juanpa8830 <jp3_4@hotmail.com>
2023-04-07 11:30:03 -0500

    Refactor comment test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  author_uno = User.new(name: 'Juan', photo: 'http://jun.png', bio: 'Engineer', posts_counter: 5)
-  post_uno = Post.new(author: author_uno, title: 'First post', text: 'Lorem Ipsum', comments_counter: 2,
+  author_uno = User.create(name: 'Juan', photo: 'http://jun.png', bio: 'Engineer', posts_counter: 5)
+  post_uno = Post.create(author: author_uno, title: 'First post', text: 'Lorem Ipsum', comments_counter: 0,
                       likes_counter: 0)
 
-  context 'When testing update_like_counter method' do
-    it 'should update the post likesCounter after create a like' do
-      Like.create(post: post_uno, author: author_uno)
-      expect(post_uno.comments_counter).to eq 2
+  context 'When testing update_comment_counter method' do
+    it 'should update the post comments_counter after create a comment' do
+      Comment.create(post: post_uno, author: author_uno, text:'hello world')
+      Comment.create(post: post_uno, author: author_uno, text:'hello world')
+      Comment.create(post: post_uno, author: author_uno, text:'hello world')
+      expect(post_uno.comments_counter).to eq 3
     end
   end
 end

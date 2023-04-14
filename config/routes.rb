611b1db90a989ef6f913f39d5e2d8a5d3a6e4440
Juanpa8830 <jp3_4@hotmail.com>
2023-04-14 17:24:31 -0500

    Refactor routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   
-  resources :users do
-    resources :posts do
-      resources :comments, :likes
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :create, :new] do
+      resources :comments, only: [:create, :new] 
+      resources :likes, only: [:create]
     end
   end
 

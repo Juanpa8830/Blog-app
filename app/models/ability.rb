073c4b90e17c236b0a98e3e9128676ee73adc3b8
dadadon <davianberoni@yahoo.com>
2023-04-20 10:18:30 -0500

    Add ability.rb configuration

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,17 @@
 class Ability
   include CanCan::Ability
+  
+  def initialize(user)
+    user ||= User.new
+
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Post, :all
+      can :destroy, Post, author_id: user.id
+      can :create, Comment, :all
+      can :destroy, Comment, author_id: user.id
+    end
+  end
 end
